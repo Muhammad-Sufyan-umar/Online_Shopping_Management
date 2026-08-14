23351341b852a9631eeff762ae742b2b1f3f589b
Muhammad-Sufyan-umar <sufyanumer04@gmail.com>
2026-08-14 16:07:22 +0500

    Add Product and Customer classes with cart functionality

---
+++ Main.py
@@ -0,0 +1,30 @@
+class Product:
+    def __init__(self,name ,price):
+        self.name=name
+        self.price=price
+
+    def show_product(self):
+        print(f"Product Name: {self.name}, Price : {self.price}")
+
+    class Customer:
+        def __init__(self,name):
+            self.name=name
+            self.cart=[]
+
+        def add_to_cart(self,product):
+            self.cart.append(product)
+            print(f"{product.name} added to cart.")
+
+        def show_cart(self):
+            print("\n-----Cart-----")
+
+            if not self.cart:
+                print("Cart is Empty")
+                return
+
+            total=0
+
+            for product in self.cart:
+                product.show_product()
+                total+=product.price
+            print(f"Total Price: {total}")
